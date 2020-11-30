5daa182538e81995c244f0b449f635b315dbaf22
OlivierFrancois <olivier.francois09@gmail.com>
2020-11-30 09:29:43 +0100

    Affichage des départements au survol

--- app.js
+++ app.js
@@ -21,29 +21,53 @@ window.onload = async function () {
     */
 
     // Définition du Canvas
+    let options = {
+        width: 700,
+        height: 700,
+        border: 'solid black 1px'
+    }
     let width = 700;
     let height = 700;
     let canvas = d3.select('#container-global-data')
         .append('svg')
-        .attr('width', width)
-        .attr('height', height);
+        .attr('width', options.width)
+        .attr('height', options.height)
+        .attr('style', 'border: ' + options.border)
     
     // Chargement de la map geojson
     let map = await d3.json('france.geojson');
     console.log(map);
+
+    // Définition d'un tooltip
+    let tooltip = d3.select("body").append("div")   
+        .attr("class", "tooltip")               
+        .style("opacity", 0);
 
     // Association des données de la map à des éléments "g" du DOM
     let group = canvas.selectAll('g')
         .data(map.features)
         .enter()
         .append('g')
+        .on("mouseover", (d) => {
+            tooltip.transition()        
+                .duration(200)
+                .style("opacity", .9);      
+            tooltip.html("Département : " + d.properties.nom)  
+                .style("left", (d3.event.pageX + 30) + "px")     
+                .style("top", (d3.event.pageY - 30) + "px")
+        })
+        .on("mouseout", function(d) {
+            tooltip.style("opacity", 0);
+            tooltip.html("")
+                .style("left", "-500px")
+                .style("top", "-500px");
+        });
 
     // Mode de projection
     let projection = d3.geoMercator()
-        .scale(2000) 
-        .rotate([0, 0]) 
-        .center([0, 0]) 
-        .translate([width / 2, height * 3]); 
+        .center([2.454071, 46.279229])
+        .scale(2600)
+        .translate([width / 2, height / 2]);
     
     // Application de la projection et dessin
     let path = d3.geoPath().projection(projection);
@@ -51,4 +75,5 @@ window.onload = async function () {
         .attr('d', path)
         .attr('class', 'area')
         .attr('fill', 'steelblue')
+
 }
